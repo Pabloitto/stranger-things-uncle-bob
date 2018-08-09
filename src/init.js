01d498f4bf2c8d692c93e72bc55f453dc9138179
Pablo Itto <pablo.itto@unosquare.com>
2018-08-08 19:35:20 -0700

    First commit

---
+++ src/init.js
@@ -0,0 +1,39 @@
+const BobChallenge = passwordSystem => {
+  const getFourDigits = (...digits) => digits.join('')
+
+  const checkPasswordMatch = fourDigitPassword => passwordSystem === fourDigitPassword
+  const burstPassword = () => {
+    let fourDigitPassword = null
+    for (let i = 0; i <= 9; i++) {
+      for (let j = 0; j <= 9; j++) {
+        for (let k = 0; k <= 9; k++) {
+          for (let l = 0; l <= 9; l++) {
+            fourDigitPassword = getFourDigits(i, j, k, l)
+            if (checkPasswordMatch(fourDigitPassword) === true) {
+              return fourDigitPassword
+            }
+          }
+        }
+      }
+    }
+    return null
+  }
+
+  return {
+    burstPassword
+  }
+}
+
+const generateSystemPassword = () => {
+  return Array(4).fill().map(item => ~~(Math.random() * 9) + 1).join('')
+}
+
+const password = generateSystemPassword()
+
+const instance = BobChallenge(password)
+
+const passwordFound = instance.burstPassword()
+
+console.log('System password', password)
+
+console.log(passwordFound ? `Password found: ${passwordFound}` : 'Password not found')
